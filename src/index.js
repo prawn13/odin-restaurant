6d1c2c2dcea5fd2e11b4ba736ca78ed0eb43c9b5
penelope davis <penelopesdavis@gmail.com>
2022-10-26 22:46:20 -0400

    Add basic buttons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import {about} from './pages/about.js';
 import {home} from './pages/home.js';
 import {menu} from './pages/menu.js';
 
+function buildNav() {
+    const navBar = document.createElement('div');
+    navBar.classList.add('nav-bar');
+
+    const homeContainer = document.createElement('div');
+    const homeButton = document.createElement('button');
+    homeButton.classList.add('nav-button');
+    homeButton.innerText = 'Home';
+    homeButton.addEventListener('click', home());
+    homeContainer.appendChild(homeButton);
+
+    const aboutContainer = document.createElement('div');
+    const aboutButton = document.createElement('button');
+    aboutButton.classList.add('nav-button');
+    aboutButton.innerText = 'About';
+    aboutButton.addEventListener('click', about());
+    aboutContainer.appendChild(aboutButton);
+
+    const menuContainer = document.createElement('div');
+    const menuButton = document.createElement('button');
+    menuButton.classList.add('nav-button');
+    menuButton.innerText = 'Menu';
+    menuButton.addEventListener('click', menu());
+    menuContainer.appendChild(menuButton);
+
+    navBar.appendChild(homeContainer);
+    navBar.appendChild(aboutContainer);
+    navBar.appendChild(menuContainer);
+
+    return navBar;
+}
+
+document.body.appendChild(buildNav());
+
+
+
 document.body.appendChild(home());
 
 
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,3 +1,3 @@
 export function menu() {
-    return;
+    console.log('menu');
 }
